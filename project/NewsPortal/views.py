a9e17d741ac5830d8625b7a9820ff665abb01c2f
Alexandr <Foma26199622@mail.ru>
2023-02-12 16:08:44 +0300

    Initial Commit

--- project/NewsPortal/views.py
+++ project/NewsPortal/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 
 class PostList(ListView):
     model = Post
-    ordering = 'heading'
+    ordering = '-date_of_creation'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['now_time'] = datetime.utcnow()
+    #     return context
+
+
+class PostDetail(DetailView):
+    model = Post
     template_name = 'post.html'
     context_object_name = 'post'
--- project/project/urls.py
+++ project/project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('NewsPortal.urls'))
+    path('news/', include('NewsPortal.urls'))
 ]
