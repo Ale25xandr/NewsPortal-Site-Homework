67783b7d7cc7a4849ef53288fdf62b4421582d3d
Alexandr <Foma26199622@mail.ru>
2023-02-16 22:57:15 +0300

    Initial Commit

--- project/NewsPortal/templatetags/custom_filters.py
+++ project/NewsPortal/templatetags/custom_filters.py
@@ -1,10 +1,34 @@
+import pymorphy2
+
 from django import template
 
-
 register = template.Library()
+
+morph = pymorphy2.MorphAnalyzer()
+
+d_1 = []
+
+forbidden_words = ['астроном', 'гороскоп', 'дед', 'пиво', 'Телевизор', 'Мужчина', 'драка', 'водка', 'Гренландия']
 
 
 @register.filter()
 def censor(text):
-    return text.upper()
+    for i in forbidden_words:
+        p = morph.parse(i.lower())
+        for l in p:
+            for k in l.lexeme:
+                d_1.append(k[0])
 
+    t = list(text.split())
+
+    for i in t:
+        if i.isalpha():
+            if i.lower() in d_1:
+                t[t.index(i)] = i.replace(i[0:len(i) - 2:1], '*' * len(i[0:len(i) - 2:1]))
+        else:
+            if i.lower()[1:len(i):1] in d_1:
+                t[t.index(i)] = i[0] + i[1:len(i):1].replace(i[1:len(i) - 2:1], '*' * len(i[1:len(i) - 2:1]))
+            if i.lower()[0:len(i) - 1:1] in d_1:
+                t[t.index(i)] = i[1:len(i):1].replace(i[1:len(i) - 2:1], '*' * len(i[1:len(i) - 2:1]))
+
+    return " ".join(t)
